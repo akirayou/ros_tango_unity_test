2282b434edf46f766c30b44a3482615418d570bd
akira_you <you.akira.noda@gmail.com>
2017-10-07 20:50:50 +0900

    Broadcast TF data with tango camera position

--- Assets/RosManager.cs
+++ Assets/RosManager.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using Ros_CSharp;
 using tf.net;
-using gm = Messages.geometry_msgs;
+using GM = Messages.geometry_msgs;
 
 
 
@@ -11,15 +11,13 @@ using gm = Messages.geometry_msgs;
 
 
 public class RosManager : MonoBehaviour {
-    NodeHandle n;
-    Transformer tf;
-    Publisher<Messages.tf.tfMessage> tf_pub;
-
-    List<gm.TransformStamped> updateList=new List<gm.TransformStamped>();
-
-
+    public Transform TangoCamera;
+    int FrameSkip = 4; //Broadcasting TF slow down factor
     //set ROS HOST It's usefule for multi NIC environmet(such as have ROS network and Internet)
-    private void setROS_HOST()
+    /// <summary>
+    /// set Network configration (ROS_MASTER_URI ,ROS_IP  and so on)
+    /// </summary>
+    void setROS_HOST()
     {
         string hostname = System.Net.Dns.GetHostName();
         //ROS.ROS_HOSTNAME = hostname; //In many case network for ROS have invalid name... I Dont't use this.
@@ -27,72 +25,141 @@ public class RosManager : MonoBehaviour {
         foreach (System.Net.IPAddress address in adrList)
         {
             if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork &&
-                address.ToString().Substring(0, 3) == "10.") {  //TODO: to select NIC for ROS. This sample means 10.0.0.0/8
+                address.ToString().Substring(0, 11) == "192.168.56.") //Have VirtualBox Ubuntu
+            {  // to select NIC for ROS. This sample means 10.0.0.0/8
                 ROS.ROS_IP = address.ToString();
+                ROS.ROS_MASTER_URI = "http://192.168.56.101:11311";
+                break;//Priority connectin have break
+            }
+            if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork &&
+                address.ToString().Substring(0, 10) == "192.168.1.") //Have VirtualBox Ubuntu
+            {  // to select NIC for ROS. This sample means 10.0.0.0/8
+                ROS.ROS_IP = address.ToString();
+                ROS.ROS_MASTER_URI = "http://192.168.1.212:11311";
+                //Not priority connection dose not have break
+            }
+            if (address.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork &&
+                address.ToString().Substring(0, 3) == "10.")
+            {  // to select NIC for ROS. This sample means 10.0.0.0/8
+                ROS.ROS_IP = address.ToString();
+                ROS.ROS_MASTER_URI = "http://10.0.1.101:11311";
             }
         }
-        //Debug.Log("ROS my HostName:" + ROS.ROS_HOSTNAME);
-        Debug.Log("ROS my HostIP  :" + ROS.ROS_IP);
     }
+
+
+    /// <summary>
+    /// Poll coordinate data and push to TF data (User Logic is here)
+    /// </summary>
+    void Poll()
+    {
+        GM.TransformStamped tfdata = BlankTf();
+        tfdata.header.frame_id = "tango_base";
+        tfdata.child_frame_id = "tango_device";
+        tfdata.transform.translation.x = TangoCamera.position.x;
+        tfdata.transform.translation.y = TangoCamera.position.y;
+        tfdata.transform.translation.z = TangoCamera.position.z;
+        tfdata.transform.rotation.x = TangoCamera.rotation.x;
+        tfdata.transform.rotation.y = TangoCamera.rotation.y;
+        tfdata.transform.rotation.z = TangoCamera.rotation.z;
+        tfdata.transform.rotation.w = TangoCamera.rotation.w;
+        AddTf(tfdata);
+
+        //FYI
+        //to deepcopy tf data,use fllowing.
+        //tfdata_to.Deserialize(tfdata_from.Serialize());
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+    //ROS Rlated member
+    NodeHandle n;
+    Transformer tf;
+    Publisher<Messages.tf.tfMessage> tf_pub;
+    List<GM.TransformStamped> BroadcastList = new List<GM.TransformStamped>();
 
     RosManager()
     {
-
-    }    
+    }
     void Awake()
     {
-        setROS_HOST();
-        ROS.ROS_MASTER_URI = "http://10.0.1.101:11311";
-        string[] dummy = { "" };
-        ROS.Init(dummy, "unity");
-        n = new NodeHandle();
-        tf = new Transformer(false/* do Interprate ?*/);
-        tf_pub = n.advertise<Messages.tf.tfMessage>("/tf", 100);
+     
     }
-
     // Use this for initialization
     void Start () {
+        setROS_HOST();
+        string[] dummy = { "" };
+        ROS.Init(dummy, "unity");//TODO:set your own node name
+        n = new NodeHandle();
+        tf = new Transformer(false/* do Interprate ?*/); /* for TF lookup*/
+        tf_pub = n.advertise<Messages.tf.tfMessage>("/tf", 100); /* for TF broad cast */
 
+        Debug.LogError("ROS my HostIP  :" + ROS.ROS_IP + "\nMASTER_URI" + ROS.ROS_MASTER_URI);
+    }
+    /// <summary>
+    /// get blank TransformStamped all member is filled (no null pointer)
+    /// you can just fill members such as x,y,z 
+    /// </summary>
+    /// <returns>Blank TransformStamped data for addTf</returns>
+    public GM.TransformStamped BlankTf()
+    {
+        GM.TransformStamped tfdata = new GM.TransformStamped();
+        tfdata.header = new Messages.std_msgs.Header();
+        tfdata.transform = new GM.Transform();
+        tfdata.header.stamp = ROS.GetTime();
+        tfdata.transform = new GM.Transform();
+        tfdata.transform.translation = new GM.Vector3();
+        tfdata.transform.rotation = new GM.Quaternion();
+        return tfdata;
     }
 
 
-    // Update is called once per frame
-    void Update () {
+    uint tfSeq = 0;
+    /// <summary>
+    /// add TF data, they will publish next Update().
+    /// </summary>
+    /// <param name="tfdata"></param>
+    public void AddTf(GM.TransformStamped tfdata)
+    {
+        lock (BroadcastList)
+        {
 
-
-
-
-
-        gm.TransformStamped tfdata = new gm.TransformStamped();
-        tfdata.header = new Messages.std_msgs.Header();
-        tfdata.transform = new gm.Transform();
-        tfdata.header.stamp = ROS.GetTime();
-        tfdata.transform = new gm.Transform();
-        tfdata.transform.translation = new gm.Vector3();
-        tfdata.transform.rotation = new gm.Quaternion();
-
-
-        tfdata.header.frame_id = "tango_base";
-        tfdata.child_frame_id = "tango_device";
-        tfdata.transform.translation.x = 0;
-        tfdata.transform.translation.y = 1;
-        tfdata.transform.translation.z = 2;
-        tfdata.transform.rotation.w = 1;
-        tfdata.transform.rotation.x = 0;
-        tfdata.transform.rotation.y = 0;
-        tfdata.transform.rotation.z = 0;
-        
-
-
-
-        Messages.tf.tfMessage tfm = new Messages.tf.tfMessage();
-        tfm.transforms = new gm.TransformStamped[] { tfdata };
-
-        tf_pub.publish(tfm);
-
-
+            
+            tfdata.header.seq = tfSeq;
+            BroadcastList.Add(tfdata);
+            tfSeq++;
 
         }
-
     }
+
+    int pollCount = 0;
+    // Update is called once per frame
+    void Update() {
+        pollCount++;
+        if (pollCount > FrameSkip)
+        {
+            pollCount = 0;
+
+            Poll();
+            lock (BroadcastList)
+            {
+                if (BroadcastList.Count != 0)
+                {
+                    Messages.tf.tfMessage tfm = new Messages.tf.tfMessage();
+                    tfm.transforms = BroadcastList.ToArray();
+                    tf_pub.publish(tfm);
+                    BroadcastList.Clear();
+                }
+            }
+        }
+     }
 }
